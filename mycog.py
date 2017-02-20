a03c77024d3b836df971f252969ac25f9a2e6c84
MGStan0 <mgmgstan@gmail.com>
2017-02-19 20:26:37 -0600

    test cog

---
+++ mycog.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+class Mycog:
+    """My custom cog that does stuff!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def playnow(self, user : discord.Member):
+        """This does stuff!"""
+
+        #Your code will go here
+        await self.bot.say("ONE PUNCH! And " + user.mention + " is out!")
+
+def setup(bot):
+    bot.add_cog(Mycog(bot))
