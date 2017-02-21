c05a1d2b5920f134d5e689fca8141724610013a6
MGStan0 <mgmgstan@gmail.com>
2017-02-20 23:24:56 -0600

    hail amry

--- test-cog/Mycog.py
+++ test-cog/Mycog.py
@@ -1,5 +1,24 @@
 import discord
 from discord.ext import commands
+import threading
+import os
+from random import shuffle, choice
+from cogs.utils.dataIO import dataIO
+from cogs.utils import checks
+from cogs.utils.chat_formatting import pagify
+from urllib.parse import urlparse
+from __main__ import send_cmd_help, settings
+from json import JSONDecodeError
+import re
+import logging
+import collections
+import copy
+import asyncio
+import math
+import time
+import inspect
+import subprocess
+from .Audio import *
 
 class Mycog:
     """My custom cog that does stuff!"""
@@ -8,10 +27,14 @@ class Mycog:
         self.bot = bot
 
 
-    @commands.command(pass_context=True)
+    @commands.group(pass_context=True)
     async def playnow(self, ctx):
         """Stops playback then starts new local playlist!"""
+        await self.bot.say('Attempting to run...')
 
+
+
+"""
         #get functions from the audio cog
         try:
             audio = self.bot.get_cog('Audio')
@@ -19,12 +42,13 @@ class Mycog:
             await self.bot.say('The Audio cog is not loaded')
 
         #stop current music and clear queue
-        await self.bot.say('Attempting to run...')
+
         audio.stop()
         await self.bot.say('Stopping now and...')
 
         audio.local(ctx)
         await self.bot.say('playing local playlist!')
+        """
 
 def setup(bot):
     bot.add_cog(Mycog(bot))
