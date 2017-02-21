2c7f45ef4202bd0650fd4ac76394b836efe6aecf
MGStan0 <mgmgstan@gmail.com>
2017-02-20 22:40:05 -0600

    fingers crossed

--- test-cog/Mycog.py
+++ test-cog/Mycog.py
@@ -1,18 +1,24 @@
 import discord
 from discord.ext import commands
 
-class Mycog:
+class playnow:
     """My custom cog that does stuff!"""
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def mycom(self):
-        """This does stuff!"""
+    @commands.group()
+    async def playnow(self, ctx):
+        """Stops playback then starts new track!"""
+        audio = self.bot.get_cog(Audio)
 
         #Your code will go here
-        await self.bot.say("I can do stuff!")
+
+        audio.Audio.stop()
+        await self.bot.say("Stopping now and...")
+
+        audio.Audio.local(ctx)
+        await self.bot.say("playing local playlist!")
 
 def setup(bot):
     bot.add_cog(Mycog(bot))
