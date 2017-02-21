1ba6a1399b8b25a3c370feb5893739638fdd71c8
MGStan0 <mgmgstan@gmail.com>
2017-02-20 23:33:16 -0600

    fix indent

--- test-cog/Mycog.py
+++ test-cog/Mycog.py
@@ -28,55 +28,55 @@ class Mycog:
 
 
 @commands.group(pass_context=True, no_pm=True)
-    async def playnow(self, ctx):
-        """Plays a local playlist"""
-        server = ctx.message.server
-        author = ctx.message.author
-        voice_channel = author.voice_channel
+async def playnow(self, ctx):
+    """Plays a local playlist"""
+    server = ctx.message.server
+    author = ctx.message.author
+    voice_channel = author.voice_channel
 
-        # Checking already connected, will join if not
+    # Checking already connected, will join if not
 
-        if not self.voice_connected(server):
-            try:
-                self.has_connect_perm(author, server)
-            except AuthorNotConnected:
-                await self.bot.say("You must join a voice channel before I can"
-                                   " play anything.")
-                return
-            except UnauthorizedConnect:
-                await self.bot.say("I don't have permissions to join your"
-                                   " voice channel.")
-                return
-            except UnauthorizedSpeak:
-                await self.bot.say("I don't have permissions to speak in your"
-                                   " voice channel.")
-                return
-            else:
-                await self._join_voice_channel(voice_channel)
-        else:  # We are connected but not to the right channel
-            if self.voice_client(server).channel != voice_channel:
-                pass  # TODO: Perms
-
-        # Checking if playing in current server
-
-        if self.is_playing(server):
-            await self.bot.say("I'm already playing a song on this server!")
-            return  # TODO: Possibly execute queue?
-
-        # If not playing, spawn a downloader if it doesn't exist and begin
-        #   downloading the next song
-
-        if self.currently_downloading(server):
-            await self.bot.say("I'm already downloading a file!")
+    if not self.voice_connected(server):
+        try:
+            self.has_connect_perm(author, server)
+        except AuthorNotConnected:
+            await self.bot.say("You must join a voice channel before I can"
+                               " play anything.")
             return
-
-        lists = self._list_local_playlists()
-
-        if not any(map(lambda l: os.path.split(l)[1] == name, lists)):
-            await self.bot.say("Local playlist not found.")
+        except UnauthorizedConnect:
+            await self.bot.say("I don't have permissions to join your"
+                               " voice channel.")
             return
+        except UnauthorizedSpeak:
+            await self.bot.say("I don't have permissions to speak in your"
+                               " voice channel.")
+            return
+        else:
+            await self._join_voice_channel(voice_channel)
+    else:  # We are connected but not to the right channel
+        if self.voice_client(server).channel != voice_channel:
+            pass  # TODO: Perms
 
-        self._play_local_playlist(server, name)
+    # Checking if playing in current server
+
+    if self.is_playing(server):
+        await self.bot.say("I'm already playing a song on this server!")
+        return  # TODO: Possibly execute queue?
+
+    # If not playing, spawn a downloader if it doesn't exist and begin
+    #   downloading the next song
+
+    if self.currently_downloading(server):
+        await self.bot.say("I'm already downloading a file!")
+        return
+
+    lists = self._list_local_playlists()
+
+    if not any(map(lambda l: os.path.split(l)[1] == name, lists)):
+        await self.bot.say("Local playlist not found.")
+        return
+
+    self._play_local_playlist(server, name)
 
 """
         #get functions from the audio cog
