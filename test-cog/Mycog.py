0b58ea43ef297b551634d964583a009c175ba75b
MGStan0 <mgmgstan@gmail.com>
2017-02-20 22:50:29 -0600

    check for audio cog

--- test-cog/Mycog.py
+++ test-cog/Mycog.py
@@ -7,18 +7,24 @@ class Mycog:
     def __init__(self, bot):
         self.bot = bot
 
+
     @commands.group()
     async def playnow(self, ctx):
         """Stops playback then starts new local playlist!"""
-        audio = self.bot.get_cog(Audio)
 
-        #Your code will go here
+        #get functions from the audio cog
+        try:
+            audio = self.bot.get_cog('Audio')
+        except:
+            await self.bot.say('The Audio cog is not loaded')
+
+        #stop current music and clear queue
 
         audio.Audio.stop()
-        await self.bot.say("Stopping now and...")
+        await self.bot.say('Stopping now and...')
 
         audio.Audio.local(ctx)
-        await self.bot.say("playing local playlist!")
+        await self.bot.say('playing local playlist!')
 
 def setup(bot):
     bot.add_cog(Mycog(bot))
